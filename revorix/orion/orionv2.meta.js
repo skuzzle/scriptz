ecbc10a680f15e39eb451edaf86dc178b77a5bc0
Simon Taddiken <simon.taddiken@gmail.com>
2014-12-31 11:40:59 +0100

    Add Orion Script

---
+++ revorix/orion/orionv2.meta.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        Polly Orion V2
+// @version     1.6
+// @description Polly Revorix Integration
+// @grant 	    GM_setValue
+// @grant 	    GM_getValue
+// @grant 	    GM_deleteValue
+// @grant 	    GM_xmlhttpRequest
+// @downloadURL https://github.com/skuzzle/scriptz/raw/master/revorix/orion/orionv2.user.js
+// @updateURL   https://github.com/skuzzle/scriptz/raw/master/revorix/orion/orionv2.meta.js
+// @namespace   projectpolly.de
+// @require     http://code.jquery.com/jquery-1.10.2.min.js
+// @require     http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js
+// @include     http://www.revorix.info/*/map.php*
+// @include     http://www.revorix.info/*/news.php*
+// @include     http://www.revorix.info/*/rx.php?set=5&fid=*
+// @include     http://www.revorix.info/*/rx.php?set=6*
+// @include     http://www.revorix.info/*/rx.php?set=3*
+// @include     http://www.revorix.de*
+// @include     http://www.revorix.info/login/
+// @include     http://www.revorix.info/php/map_fflotte.php?fid=*
+// @include     http://www.revorix.info/php/venad_list.php?pktsur=1
+// @include     http://www.revorix.info/php/venad_list.php?pkttop=1
+// @include     http://www.revorix.info/php/setup.php
+// @include     http://www.revorix.info/php/news_pop.php*
+// @include		http://www.revorix.info/php/map_attack.php?fida=*
+// @include		http://www.revorix.info/php/handel_all.php*
+// @include		http://www.revorix.info/php/handel_eigen.php*
+// ==/UserScript==
