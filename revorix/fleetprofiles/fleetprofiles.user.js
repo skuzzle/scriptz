8b86165f33a82143e1189b835062bf7e0bc1cb81
Simon Taddiken <simon.taddiken@gmail.com>
2015-07-21 07:54:57 +0200

    Do not use meta file for updating anymore

--- revorix/fleetprofiles/fleetprofiles.user.js
+++ revorix/fleetprofiles/fleetprofiles.user.js
@@ -6,7 +6,7 @@
 // @grant       GM_getValue
 // @grant       GM_deleteValue
 // @downloadURL https://github.com/skuzzle/scriptz/raw/master/revorix/fleetprofiles/fleetprofiles.user.js
-// @updateURL   https://github.com/skuzzle/scriptz/raw/master/revorix/fleetprofiles/fleetprofiles.meta.js
+// @updateURL   https://github.com/skuzzle/scriptz/raw/master/revorix/fleetprofiles/fleetprofiles.user.js
 // @namespace   projectpolly.de
 // @require     http://code.jquery.com/jquery-1.10.2.min.js
 // @include     http://www.revorix.info/php/schiff_portal.php*
